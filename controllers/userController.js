d9e240dc796a7b1a9a674b59fd3e315ea4f8ba32
deep56-dot <72162650+deep56-dot@users.noreply.github.com>
2024-09-26 01:06:36 +0530

    bug fix in controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,49 +1,105 @@
 
 const userModel= require("../models/userModel")
+const bcrypt = require("bcrypt")
 // login callback
 const loginController=async (req,res)=>{
-    try {
-        const {email,password}=req.body;
-        const user=await userModel.findOne({email,password});
-        if(!user){
-            return res.status(404).send("user Not Found");
-        }
-        console.log("login ")
-        res.status(200).json({
-            success:true,
-            user
-        });
-    }
-     catch (error) {
-        console.log("login error")
+    try{
+        const { email, password } = req.body;
 
-        res.status(400).json({
-            success:false,
-            error
+        // console.log(email, password);
+  
+        if (!email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Please enter All Fields",
+            }); 
+        }
+    
+        const user = await userModel.findOne({ email });
+    
+        if (!user){
+            return res.status(401).json({
+                success: false,
+                message: "User not found",
+            }); 
+        }
+    
+        const isMatch = await bcrypt.compare(password, user.password);
+    
+        if (!isMatch){
+            return res.status(401).json({
+                success: false,
+                message: "Incorrect Email or Password",
+            }); 
+        }
+
+        delete user.password;
+
+        return res.status(200).json({
+            success: true,
+            message: `Welcome back, ${user.name}`,
+            user,
+        });
+
+    }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: err.message,
         });
     }
 }
 
 //register callback
 const registerController=async (req,res)=>{
-    try {
-        console.log(req.body)
-        const newUser= new userModel(req.body);
-        await newUser.save();
-        console.log("register ")
+    try{
+        const {name, email, password} = req.body;
 
-        res.status(201).json({
-            success:true,
-            newUser
+        // console.log(name, email, password);
+
+        if(!name || !email || !password){
+            return res.status(400).json({
+                success: false,
+                message: "Please enter All Fields",
+            }) 
+        }
+
+        let user = await userModel.findOne({email});
+
+        if(user){
+            return res.status(409).json({
+                success: false,
+                message: "User already Exists",
+            });
+        }
+
+        
+
+        const salt = await bcrypt.genSalt(10);
+
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        // console.log(hashedPassword);
+
+        let newUser = await userModel.create({
+            name, 
+            email, 
+            password: hashedPassword, 
         });
-    } catch (error) {
-        console.log("register error")
 
-        res.status(400).json({
-            success:false,
-            error
+        return res.status(200).json({
+            success: true,
+            message: "User Created Successfully",
+            user: newUser
         });
     }
+    catch(err){
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        });
+    }
+
 }
 
 module.exports={loginController,registerController}
